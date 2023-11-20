627da6fed4b89c0a24aa497d1047b0da49400685
Estefania <esm981016@outlook.es>
2023-11-20 17:50:59 -0500

    Inicio curso Node:JS

---
+++ monohilo.js
@@ -0,0 +1,11 @@
+console.log("Hola mundo");
+
+// setInterval(function(){
+//     console.log ("Sigo activo");
+// }, 2000);
+
+// let i=0;
+// setInterval(function(){
+//     console.log(i);
+//     i++;
+// },2000);
